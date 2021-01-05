4b95708a899f6e0791527c46246a2352d77d225b
Mel Espiloy <espiloy.mel11@gmail.com>
2021-01-05 21:30:52 +0800

    Added google fonts to index. html and style.css && git push

--- index.php
+++ index.php
@@ -19,7 +19,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To Do List</title>
     <link rel = "stylesheet" type = "text/css" href="style.css?v=<?php echo time(); ?>">
-
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <div class="size">
+        <img class ="image" src="Logo.png" alt="">
+        <h1 class = "header">To do list</h1>
+    </div>
 </head>
 <body>
     
